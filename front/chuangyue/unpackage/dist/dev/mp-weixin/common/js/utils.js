f1e37dfd2afd472be3a55ecf8aa9e381893a9afd
zyp <2075504518@qq.com>
2025-04-14 22:24:14 +0800

    停止跟踪 front/chuangyue/unpackage 目录

--- front/chuangyue/unpackage/dist/dev/mp-weixin/common/js/utils.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-const baseUrl = "http://localhost:8080";
-const wsUrl = "ws://localhost:8080";
-exports.baseUrl = baseUrl;
-exports.wsUrl = wsUrl;
--- front/chuangyue/unpackage/dist/dev/mp-weixin/pages/about/about.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-const common_vendor = require("../../common/vendor.js");
-const _sfc_main = {};
-function _sfc_render(_ctx, _cache) {
-  return {};
-}
-const MiniProgramPage = /* @__PURE__ */ common_vendor._export_sfc(_sfc_main, [["render", _sfc_render], ["__scopeId", "data-v-13a78ac6"], ["__file", "D:/毕业设计/projects/front/chuangyue/pages/about/about.vue"]]);
-wx.createPage(MiniProgramPage);
